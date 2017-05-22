c1816c994f4b72171e91b3e51976b9d6111ab750
Simon Hoye <simonhoye@gmail.com>
2017-05-22 19:51:43 +1000

    Added some presentation components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
     state = { albums: [] };
@@ -10,11 +11,19 @@ class AlbumList extends Component {
             .then((responseData) => this.setState({ albums: responseData }));
     }
 
+    renderAlbums() {
+        return this.state.albums.map(album => 
+        <AlbumDetail 
+            key={album.title}
+            album={album}
+        />);
+    }
+
     render() {
         console.log(this.state);
         return (
             <View>
-                <Text>Album List!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
